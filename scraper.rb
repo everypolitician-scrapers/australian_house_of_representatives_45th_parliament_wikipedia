99f4ec93f475efb233c508a99ac215fb5177d851
Henare Degan <henare.degan@gmail.com>
2017-11-28 11:34:37 +1100

    Scrape page into a blank MembersPage scraped class

--- scraper.rb
+++ scraper.rb
@@ -8,3 +8,16 @@ require 'wikidata_ids_decorator'
 
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
+
+class MembersPage < Scraped::HTML
+  field :members do
+    # TODO
+  end
+end
+
+url = 'https://en.wikipedia.org/wiki/Members_of_the_Australian_House_of_Representatives,_2016%E2%80%932019'
+page = MembersPage.new(response: Scraped::Request.new(url: url).response)
+data = page.members.map(&:to_h)
+
+ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
+# ScraperWiki.save_sqlite([:name, :wikidata], data)
